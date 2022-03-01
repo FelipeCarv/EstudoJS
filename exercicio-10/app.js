f61ba3e0b591e52981b39242b93f587e13f5503b
Felipe Carvalho <52979989+FelipeCarv@users.noreply.github.com>
2022-03-01 15:33:43 -0300

    Add files via upload

---
+++ exercicio-10/app.js
@@ -0,0 +1,136 @@
+/*
+  01
+
+  - Crie um objeto "cat" que possui as seguintes propriedades:
+    - name, que recebe o nome do gato;
+    - age, que recebe a idade;
+    - color, que recebe a cor;
+    - bestFriends, que recebe um array com os melhores amigos do gato;
+    - sound, que é um método que retorna o miado do gato.
+  - Exiba o objeto no console.
+*/
+
+let cat = {
+  name: "ninha",
+  age: 1,
+  color: "Rajada cinza",
+  bestFriends: ['Gata Mãe', "Lily"],
+  sound: function(){
+    return "Meeow"
+  }
+}
+
+// console.log(cat);
+
+/*
+  02
+
+  - Exiba a mensagem abaixo no console, substituindo os "X" pelas informações  
+    corretas.
+
+  Até aqui, o objeto "cat" possui as seguintes propriedades e valores: "name", que recebeu "X", "age", que recebeu "X", "color", que recebeu "X", "bestFriends", que recebeu um array com os itens "X" e "X", e "sound", que recebeu uma função que retorna "X".
+*/
+// console.log(`Até aqui, o objeto "cat" possui as seguintes propriedades e valores: "name", que recebeu "${cat.name}", "age", que recebeu "${cat.age}", "color", que recebeu "${cat.color}", "bestFriends", que recebeu um array com os itens "${cat.bestFriends[0]}" e "${cat.bestFriends[1]}", e "sound", que recebeu uma função que retorna "${cat.sound()}".`)
+/*
+  03
+
+  - Adicione 2 anos à idade do gato e exiba a idade atualizada no console.
+*/
+ cat.age += 2;
+//  console.log(cat.age)
+/*
+  04
+
+  - Crie uma função que adiciona um amigo(a) no array de amigos do gato;
+  - Exiba o array de amigos no console para verificar se o novo amigo(a) foi  
+    adicionado.
+*/
+
+let addFriend = (value) =>{
+  cat.bestFriends.push(value);
+}
+
+addFriend("Felipe");
+// console.log(cat.bestFriends)
+
+/*
+  05
+
+  - Crie uma função que adiciona mais uma cor à cor do gato;
+  - Utilize a sintaxe de colchetes para fazer isso;
+  - Exiba a nova cor do gato no console, também utilizando a sintaxe de  
+    colchetes.
+*/
+let addColor = (value) =>{
+  cat.color += value;
+}
+
+// addColor("branco");
+// console.log(cat.color)
+/*
+  06
+
+  - Crie uma função que retorna um boolean indicando se um valor recebido por  
+    parâmetro é um objeto;
+  - Utilize a função para exibir no console se "cat" é um objeto.
+*/
+const isObject = (value) => {typeof value === 'object'}
+// console.log(isObject(cat));
+
+/*
+  07
+
+  - Crie um objeto "dog" que possui as mesmas propriedades do objeto "cat";
+  - Crie uma função que retorna a mensagem abaixo, com as informações corretas;
+  - Exiba a mensagem no console.
+
+  "A soma das idades de NOME_DO_GATO e NOME_DO_CACHORRO é RESULTADO_DA_SOMA."
+*/
+
+let dog = {
+  name: "Lily",
+  age: 1,
+  color: "Branco",
+  bestFriends: ['Gata Mãe', "Ninha"],
+  sound: function(){
+    return "Meeow"
+  }
+}
+
+let message = (ob1, ob2) => { 
+  console.log(`A soma das idades de ${ob1.name} e ${ob2.name} é ${ob1.age + ob2.age}.`)
+}
+
+// message(cat, dog);
+
+/*
+  08
+
+  - Se você descomentar os 2 console.log abaixo, verá que a função "isAnSUV"  
+    está funcionando corretamente. Mas ela pode ser melhorada;
+  - Como você refatoraria esta função?
+*/
+
+const isAnSUV = car => {
+  if (car === 'Honda HR-V' || car === 'Jeep Renegade' || car === 'Ford EcoSport' || car === 'Hyundai iX35') {
+    return true
+  }
+
+  return false
+}
+
+// console.log(isAnSUV('Honda Civic'))
+// console.log(isAnSUV('Ford EcoSport'))
+
+/*
+  09
+
+  - Crie uma função que recebe um parâmetro 'type';
+  - Dentro da função, crie um objeto com as seguintes propriedades:
+    - null, que recebe a mensagem 'Seta, explicitamente, uma variável sem valor.'
+    - undefined, que recebe a mensagem 'Representa um valor não-setado.'
+    - object, que recebe a mensagem 'Arrays, Datas, Objetos literais, Funções, etc.'
+  - Se o valor que o parâmetro type receber corresponder à alguma das 3  
+    propriedades, retorne a mensagem que a propriedade armazena;
+  - Teste a função, exibindo no console a mensagem de cada propriedade.
+*/
